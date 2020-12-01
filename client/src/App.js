9537afeb9f54729fef2a73337d3b3e9f2bb920db
Daniel Rodriguez <drod18@hotmail.com>
2020-12-01 13:17:26 -0600

    created footer component to be used on all pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
 import Login from "./pages/Login.js"
 import CardContainer from './components/CardContainer'
+import Footer from './components/Footer'
 
 function App(){
   return (
     <Router>
       <div>
-        <Navbar />
+      <Navbar/>
       <Route exact path="/" component={Home}/>
       <Route exact path="/home" component={Home}/>
       <Route exact path="/login" component={Login}/>
       <CardContainer/>
+
+      <Footer/>
       </div>
+
     </Router>
   )
 }
