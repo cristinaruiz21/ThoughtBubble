4d8ab77b0a0f37b25d02f1828ec8429608474430
satya kavya <satyakavya24@gmail.com>
2020-12-01 10:37:09 -0600

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {BrowserRouter as Router , Route} from  "react-router-dom";
+import Home from "./pages/Home"
+import Check from "./pages/checkout"
+import Navbar from "./components/Navbar"
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <div>
+        <Navbar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/home" component={Home}/>
+      <Route exact path="/logout" component={Check}/>
 
-export default App;
+
+      </div>
+    </Router>
+  )
+}
+export default App
---
+++ client/src/pages/checkout.js
@@ -0,0 +1,9 @@
+import React from "react"
+function Checkout ()
+    {
+
+return <p>This is checkout</p>
+
+
+    }
+export default Checkout
