2f9a58866a8b6a7d144e46cc2b228ac6bf3f3a12
Daniel Rodriguez <drod18@hotmail.com>
2020-12-02 11:12:54 -0600

    commenting out the other info to show thought page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,32 @@
 import React from "react";
 import {BrowserRouter as Router , Route} from  "react-router-dom";
-import Home from "./pages/Home"
+// import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
-import Login from "./pages/Login.js"
-import CardContainer from './components/CardContainer'
+// import Login from "./pages/Login.js"
+// import CardContainer from './components/CardContainer'
 import Footer from './components/Footer'
-import Signup from "./pages/Signup.js"
-import Fitness from "./pages/Fitness"
-import Food from "./pages/Food"
+// import Signup from "./pages/Signup.js"
+// import Fitness from "./pages/Fitness"
+// import Food from "./pages/Food"
 import Thoughts from "./pages/Thoughts"
+import './App.css';
 
 function App(){
   return (
     <Router>
       <div>
-      <Navbar/>
-      <Route exact path="/" component={Home}/>
+       <Navbar/>
+      {/*<Route exact path="/" component={Home}/>
         <Navbar />
       <Route exact path="/" component={Login}/>
       <Route exact path="/home" component={Home}/>
       <Route exact path="/login" component={Login}/>
       <Route exact path="/signup" component={Signup}/>
       <Route exact path="/home/fitness" component={Fitness}/>
-      <Route exact path="/home/food" component={Food}/>
-      <Route exact path="/home/thoughts" component={Thoughts}/>
-      <CardContainer/>
-
+      <Route exact path="/home/food" component={Food}/> */}
+      {/* <Route exact path="/home/thoughts" component={Thoughts}/> */}
+      {/* <CardContainer/> */}
+      <Thoughts/>  
       <Footer/>
       </div>
 
