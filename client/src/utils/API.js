41f26790ff66065c714597df1277749886d607b1
satya kavya <satyakavya24@gmail.com>
2020-12-03 15:42:39 -0600

    pull data from mongodb and show in cards

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export default {
-
+  getBubbles: function() {
+    return axios.get("/api/bubble");
+  },
 //   // Gets the bubble based on given id
-//   getBubbles: function(id) {
-//     return axios.get("/api/bubbles/" + id);
-//   },
+  // getBubbles: function(id) {
+  //   return axios.get("/api/bubbles/" + id);
+  // },
 //   // Deletes the book with the given id
 //   deleteBubble: function(id) {
 //     return axios.delete("/api/bubble/" + id);
--- controllers/bubbleController.js
+++ controllers/bubbleController.js
@@ -16,5 +16,12 @@ module.exports = {
           .then(dbModel => dbModel.remove())
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
+      },
+      findAll : function(req,res){
+        db.Bubble
+        .find(req.query)
+        .sort({date: -1})
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
       }
 };
