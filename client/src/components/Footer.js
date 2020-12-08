02b9088d9939a5bca61b0546ae4c555238070ea7
Daniel Rodriguez <drod18@hotmail.com>
2020-12-08 09:27:07 -0600

    fixed the footer to match the navbar css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,31 +4,27 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 const styles ={
     foot :{
-        // background: "gray",
-        // display :"flex",
-        // justifyContent : "center",
-        // padding : 10
+        
         position: "fixed",
         left: 0,
         bottom: 0,
         width: "100%",
         backgroundColor:"gray",
         color: "white",
-        
-        // textAlign: "center"
+        textAlign: "center",
     }
 }
 
 function Footer() {
 
     return (
-        <footer className="mt-5" style={styles.foot} >
+        <footer className="mx-auto" style={styles.foot} >
             <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                    <Col className="p-0 md={3} sm={12}">
+                <Row className="border-top p-3">
+                    <Col className="">
                     <a href="https://github.com/cristinaruiz21/ThoughtBubble" style={{color:"white"}}>Github</a>
                     </Col>
-                    <Col className="p-0 d-flex justify-content-end" md={3}>
+                    <Col className="">
                     Thought Bubble ©2020 
                     </Col>
                 </Row>
